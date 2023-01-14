a0076ff0b0af300d620b998f230e01d278b0175e
alex <colo.alexandru@yahoo.com>
2023-01-14 12:30:44 +0200

    Add main activity and register activity to the file

---
+++ app/src/main/java/com/alex/hollydayappv1/RegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.alex.hollydayappv1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_activity);
+    }
+}
