251e8f7fc796d80c1754586d5637788f513a567a
alex <colo.alexandru@yahoo.com>
2023-01-14 12:08:16 +0200

    Add main activity and register activity to the file

--- app/src/main/java/com/alex/hollydayappv1/MainActivity.java
+++ app/src/main/java/com/alex/hollydayappv1/MainActivity.java
@@ -2,13 +2,27 @@ package com.alex.hollydayappv1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.switch1);
+
+        tv.setOnClickListener(view -> {
+
+            Intent intent = new Intent(getApplicationContext(),RegistrationActivity.class);
+            startActivity(intent);
+        });
+
+
     }
 }
